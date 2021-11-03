5d1b5dd6421b099a3fd549c79c2a23db9904429e
Kenneth Kim <kkm2059@gmail.com>
2021-11-02 22:06:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+var customerName = `bob`
+function upperCaseCustomerName(){
+   return customerName.toUpperCase
+}
+
+function setBestCustomer(){
+    var bestCustomer = `not bob`;
+    return bestCustomer;
+}
+setBestCustomer();
+console.log (setBestCustomer())
+
+function overwriteBestCustomer() {_
+    var bestCustomer = `rick`
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = `todd`
+
+function changeLeastFavoriteCustomer () {
+    var leastFavoriteCustomer = 'not todd'
+    return leastFavoriteCustomer;
+}
+upperCaseCustomerName
+console.log(customerName)
